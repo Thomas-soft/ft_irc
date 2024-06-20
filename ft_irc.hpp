8becb9d2f1388d821c97cc3f5474a49131058de2
dydyjetski <dydy@DESKTOP-MHGP9LB>
2024-06-21 01:21:50 +0200

    Change atoi by ft_stoi & comment the code

---
+++ ft_irc.hpp
@@ -0,0 +1,14 @@
+#ifndef FT_IRC_HPP
+#define FT_IRC_HPP
+
+#include <string>
+#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <cstring>
+#include <stdlib.h>
+#include <sstream>
+
+#endif
